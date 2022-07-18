0e139f6b4b1891933e9690d848dfbf179c93c5fe
Jody Zhou <jody625326@gmail.com>
2022-07-18 14:54:23 -0400

    created prod, dev, and int environment for django backend

--- uwpath_backend/dev.py
+++ uwpath_backend/dev.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7ldnlu_gz*&vx&=4)q3#74ih@nzx*owgeu^=*katjqpbb-s1sk'
+SECRET_KEY = os.getenv("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
