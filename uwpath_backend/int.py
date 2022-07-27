eaa603d7e2ac4bcfa0475dcd42193d7ffc7afb7e
Jody Zhou <jody625326@gmail.com>
2022-07-27 10:29:06 -0400

    added cors whitelist

--- uwpath_backend/int.py
+++ uwpath_backend/int.py
@@ -178,4 +178,6 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
     'http://127.0.0.1:8000',
     'http://0.0.0.0:8000',
+    'http://129.153.61.57',
+    'http://parallel.uwpath.com',
 ]
