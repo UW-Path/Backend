71bb0dd71d1b2bfd7c81b6d6126a7897457545f0
William-Gao1 <william2010.gao@gmail.com>
2022-09-11 23:06:49 -0400

    added temp conditional

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
-echo $WALLET_TNSNAMES_ORA > /code/Wallet_uwpathparallel/tnsnames.ora
-echo $WALLET_SQLNET_ORA > /code/Wallet_uwpathparallel/sqlnet.ora
-echo $WALLET_CWALLET_SSO > /code/Wallet_uwpathparallel/cwallet.sso
+if [ "$GCP" = 'true' ]
+then
+    echo 'Copying wallet'
+    echo $WALLET_TNSNAMES_ORA > /code/Wallet_uwpathparallel/tnsnames.ora
+    echo $WALLET_SQLNET_ORA > /code/Wallet_uwpathparallel/sqlnet.ora
+fi
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
