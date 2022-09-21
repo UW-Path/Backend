67ae2fb028cab62bcfd3d37b7ef22e730e32f5b5
William-Gao1 <william2010.gao@gmail.com>
2022-09-21 17:22:56 -0400

    typo

--- uwpath_backend/prod_settings.py
+++ uwpath_backend/prod_settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['api.uwpath.com']
+ALLOWED_HOSTS = ['api.uwpath.com', 'uwpath-backend-prod-qtsbtvgvna-uc.a.run.app']
 
 
 # Application definition
